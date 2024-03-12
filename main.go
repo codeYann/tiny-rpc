5ac2dcc781c973e0752f03a8739e06146f015a12
Yan Avlis <ccyan05@alu.ufc.br>
2024-03-12 09:11:46 -0300

    modified:   main.go
    Hello world protobuf

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	"github.com/codeYann/tiny-rpc/pb"
+)
+
+type Server struct {
+	pb.UnimplementedHelloServer
+}
+
+func (s *Server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
+	return &pb.HelloResponse{Message: "Hello, World!"}, nil
+}
 
 func main() {
-    fmt.Println("Hello, World!");
+	fmt.Println("Running gRPC Server")
+
+	listener, err := net.Listen("tcp", "localhost:9000")
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterHelloServer(s, &Server{})
+
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("failed to serve %v", err)
+	}
 }
